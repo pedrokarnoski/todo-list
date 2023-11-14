17173081de6019eb49736820aa56fe16ba53980c
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2023-11-13 23:01:51 -0300

    List tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,11 +1,24 @@
+import { Trash } from "phosphor-react";
+
 import styles from "./Tasks.module.css";
 
 export function Tasks() {
   return (
     <div className={styles.task}>
       <div className={styles.taskContent}>
-        <input type="radio" />
-        <p>Tarefa</p>
+        <div className={styles.checkboxContainer}>
+          <input
+            type="checkbox"
+            className={styles.checkboxInput}
+            id="myCheckbox"
+          />
+          <label htmlFor="myCheckbox">
+            Descrição da tarefa
+          </label>
+        </div>
+        <button title="Deletar tarefa">
+          <Trash size={24} />
+        </button>
       </div>
     </div>
   )
